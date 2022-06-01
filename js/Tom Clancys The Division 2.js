4937ed63e0ddfb9e80e7837b04ece758e0eaa965
MuFen <3550035202@qq.com>
2022-06-01 17:21:15 +0800

    增加胸甲天赋

--- js/Tom Clancys The Division 2.js
+++ js/Tom Clancys The Division 2.js
@@ -45,8 +45,98 @@ const WeaponTalent = new Vue({
     ChGoBlind(){
       this.GoBlind = !this.GoBlind
     },
-    Ch5
+    ChInsane(){
+      this.Insane = !this.Insane
+    },
+    ChLight(){
+      this.Light = !this.Light
+    },
+    ChPCFirst(){
+      this.PCFirst = !this.PCFirst
+    },
+    ChPCLast(){
+      this.PCLast = !this.PCLast
+    },
+    ChNaked(){
+      this.Naked = !this.Naked
+    },
+    ChOS(){
+      this.OS = !this.OS
+    },
+    ChTTM(){
+      this.TTM = !this.TTM
+    },
+    ChFistToMeat(){
+      this.FistToMeat = !this.FistToMeat
+    },
+    ChFullOfEnergy(){
+      this.FullOfEnergy = !this.FullOfEnergy
+    },
+    ChRanger(){
+      this.Ranger = !this.Ranger
+    },
+    ChRifleman(){
+      this.Rifleman = !this.Rifleman
+    },
+    ChToAbuse(){
+      this.ToAbuse = !this.ToAbuse
+    },
+    ChInsanity(){
+      this.Insane = !this.Insane
+    },
+    ChAsystole(){
+      this.Asystole = !this.Asystole
+    }
   },
 })
+const BreastplateTalent = new Vue({
+    data(){
+        return{
+          FeelTheSame:'',
+          Absorbed:'',
+          GlassCannon:'',
+          Gunslinger:'',
+          Intimidate:'',
+          OF:'',
+          ShelterCover:'',
+          Spark:'',
+          HeadHunter:'',
+          Scout:''
+        }
+    },
+    methods:{
+      ChFeelTheSame(){
+        this.FeelTheSame = !this.FeelTheSame
+      },
+      ChAbsorbed(){
+        this.Absorbed = !this.Absorbed
+      },
+      ChGlassCannon(){
+        this.GlassCannon = !this.GlassCannon
+      },
+      ChGunslinger(){
+        this.Gunslinger = !this.Gunslinger
+      },
+      ChIntimidate(){
+        this.Intimidate = !this.Intimidate
+      },
+      ChOF(){
+        this.OF = !this.OF
+      },
+      ChShelterCover(){
+        this.ShelterCover = !this.ShelterCover
+      },
+      ChSpark(){
+        this.Spark = !this.Spark
+      },
+      ChHeadHunter(){
+        this.HeadHunter = !this.HeadHunter
+      },
+      ChScout(){
+        this.Scout = !this.Scout
+      }
+    }
+})
+BreastplateTalent.$mount('#BreastplateTalent')
 WeaponTalent.$mount('#WeaponTalent')
 root.$mount('#root')
