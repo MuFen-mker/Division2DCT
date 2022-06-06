bcfda7885598aacc6e419794d19026cd0e7c8a5e
MuFen <3550035202@qq.com>
2022-06-06 08:30:56 +0800

    添加过滤器实现千位分隔符

--- Tom-Clancys-The-Division-2-Injury-calculation-tool/js/Tom Clancys The Division 2.js
+++ Tom-Clancys-The-Division-2-Injury-calculation-tool/js/Tom Clancys The Division 2.js
@@ -1,3 +1,20 @@
+Vue.filter("numberToCurrency",function(value){
+  if (!value) return 0
+  // 获取整数部分
+  const intPart = Math.trunc(value)
+  // 整数部分处理，增加,
+  const intPartFormat = intPart.toString().replace(/(\d)(?=(?:\d{3})+$)/g, '$1,')
+  // 预定义小数部分
+  let floatPart = ''
+  // 将数值截取为小数部分和整数部分
+  const valueArray = value.toString().split('.')
+  if (valueArray.length === 2) { // 有小数部分
+    floatPart = valueArray[1].toString().slice(0,2) // 取得小数部分
+    return intPartFormat + '.' + floatPart
+  }
+  return intPartFormat + floatPart
+});
+
 const root = new Vue({
   data () {
     return {
@@ -770,3 +787,5 @@ const Calculation = new Vue({
 Calculation.$mount('#Calculation')
 Checkbox.$mount('#Checkbox')
 root.$mount('#root')
+
+
