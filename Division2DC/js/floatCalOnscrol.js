c01386a12f312d82268c99c72ca0372b1bc03fca
MuFen <3550035202@qq.com>
2022-06-11 19:23:10 +0800

    修正:让部分按钮不可被复制

--- Division2DC/js/floatCalOnscrol.js
+++ Division2DC/js/floatCalOnscrol.js
@@ -3,21 +3,21 @@ var floatCalDisplay;
 var allComputeButtonOffsetToTop;
 var windowsWidth;
 var temp;
-document.onscroll = function(){
+document.onscroll = function () {
   floatCalDisplay = document.getElementById('floatCal');
   allComputeButtonOffsetToTop = $('#CulationButton').offset().top;
   windowsWidth = document.documentElement.clientWidth;
   temp = document.documentElement.scrollTop;
   console.log(ButtonAnit2if);
   if (windowsWidth <= 730) {
-      if (temp > allComputeButtonOffsetToTop) {
-        floatCalDisplay.style['animation-name'] = 'ButtonAnit1';
-        ButtonAnit2if = 1;
-      } else {
-        if (ButtonAnit2if == 1) {
-          ButtonAnit2if = 0;
-          floatCalDisplay.style['animation-name'] = 'ButtonAnit2';
-        }
-      } 
+    if (temp > allComputeButtonOffsetToTop) {
+      floatCalDisplay.style['animation-name'] = 'ButtonAnit1';
+      ButtonAnit2if = 1;
+    } else {
+      if (ButtonAnit2if == 1) {
+        ButtonAnit2if = 0;
+        floatCalDisplay.style['animation-name'] = 'ButtonAnit2';
+      }
+    }
   }
 }
