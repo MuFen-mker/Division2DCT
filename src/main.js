d63794fcc19e5377b624e761f400d0336779915e
MuFen <3550035202@qq.com>
2022-06-19 14:52:47 +0800

    优化页面响应速度

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import jquery from 'jquery';
 import App from './App.vue';
 import {Autocomplete} from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
+// import 'element-ui/lib/theme-chalk/index.css';
 import {numberToCurrencyNo} from '@/utils/numberToCurrency';
 // 配置全局过滤器，实现数字千分位格式
 
